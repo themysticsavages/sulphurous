cf71f7980f8f78621635d7ca163135354521a6f7
ajskateboarder <81255936+themysticsavages@users.noreply.github.com>
2021-08-18 13:09:44 -0400

    very big update legit

--- application/app.py
+++ application/app.py
@@ -19,12 +19,31 @@ def run():
 
     @app.route('/projects/<query>')
     def project(query):
-        try:
-            search = json.loads(str(Scratch.getInfo(query)).replace("'", '"'))
-            rep = {'//views': str(search['stats']['views']), '//rem': str(search['stats']['remixes']), '//stars': str(search['stats']['favorites']), '//loves': str(search['stats']['loves']), '.id.': query, '//project-title': '{} by {}'.format(search['title'], search['author'])}
+        search = json.loads(str(Scratch.getInfo(query)).replace("'", '"'))
 
+        try:
+            comments = Scratch.getProjComments(query, 3)
+        except IndexError:
+            comments = None
+
+        rep = {'//views': str(search['stats']['views']), '//rem': str(search['stats']['remixes']), '//stars': str(search['stats']['favorites']), '//loves': str(search['stats']['loves']), '.id.': query, '//project-title': '{} by {}'.format(search['title'], search['author'])}
+        if not search['remix'] == 'False':
+                rep['//rmixstatus'] = 'Remix of {} by {}'.format(Scratch.getInfo(str(search['remix']))['title'], Scratch.getInfo(str(search['remix']))['author'])
+        else:
+                rep['//rmixstatus'] = ''
+        if comments:
+                rep['//comment1'] = '<i>{}</i>: {}<br><p style="font-size: xx-small">{}</p>'.format(comments[0][0], comments[0][1], comments[0][2])
+                rep['//comment2'] = '<i>{}</i>: {}<br><p style="font-size: xx-small">{}</p>'.format(comments[1][0], comments[1][1], comments[1][2])
+                rep['//comment3'] = '<i>{}</i>: {}<br><p style="font-size: xx-small">{}</p>'.format(comments[2][0], comments[2][1], comments[2][2])
+        else:
+                rep['//comment1'] = ''
+                rep['//comment2'] = ''
+                rep['//comment3'] = ''
+
+        try: 
             rep = dict((re.escape(k), v) for k, v in rep.items())
             pattern = re.compile("|".join(rep.keys()))
+
             text = pattern.sub(lambda m: rep[re.escape(m.group(0))], open('./html/getproject.html', encoding='utf-8').read())
 
             return text
