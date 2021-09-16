4fd492e90f29b8427d33a8ae89c0696676adaf3d
ajskateboarder <81255936+themysticsavages@users.noreply.github.com>
2021-09-16 13:19:50 -0400

    Add files via upload

--- application/app.py
+++ application/app.py
@@ -1,8 +1,11 @@
 from scratchhh.scratchhh import Scratch
 from gevent.pywsgi import WSGIServer
+from functools import update_wrapper
+from datetime import timedelta
 from .config import host, port
 from dateutil import parser
 from flask import *
+import requests
 import pathlib
 import filecmp
 import json
@@ -12,7 +15,47 @@ import re
 app = Flask(__name__)
 
 def run():
+    def crossdomain(origin=None, methods=None, headers=None, max_age=21600, attach_to_all=True, automatic_options=True):
+      if methods is not None:
+            methods = ', '.join(sorted(x.upper() for x in methods))
+      if headers is not None and not headers:
+            headers = ', '.join(x.upper() for x in headers)
+      if not origin:
+            origin = ', '.join(origin)
+      if isinstance(max_age, timedelta):
+            max_age = max_age.total_seconds()
+
+      def get_methods():
+            if methods is not None:
+                  return methods
+
+            options_resp = current_app.make_default_options_response()
+            return options_resp.headers['allow']
+
+      def decorator(f):
+            def wrapped_function(*args, **kwargs):
+                  if automatic_options and request.method == 'OPTIONS':
+                        resp = current_app.make_default_options_response()
+                  else:
+                        resp = make_response(f(*args, **kwargs))
+                  if not attach_to_all and request.method != 'OPTIONS':
+                        return resp
+
+                  h = resp.headers
+
+                  h['Access-Control-Allow-Origin'] = origin
+                  h['Access-Control-Allow-Methods'] = get_methods()
+                  h['Access-Control-Max-Age'] = str(max_age)
+                  if headers is not None:
+                        h['Access-Control-Allow-Headers'] = headers
+                  return resp
+
+            f.provide_automatic_options = False
+            return update_wrapper(wrapped_function, f)
+      return decorator
+
     @app.route('/')
+    @crossdomain(origin='*')
     def home():
         return '{}'.format(open('./html/index.html', encoding='utf-8').read())
 
@@ -36,6 +79,10 @@ def run():
             rep['//description'] = search['desc']
             rep['<!--1'] = ''
             rep['1-->'] = ''
+
+          if os.path.isdir('./projcache/{}'.format(query)):
+            rep['<!--0'] = ''
+            rep['0-->'] = ''
       
           if comments:
             try:
@@ -109,6 +156,7 @@ def run():
             return open('./html/404.html', encoding='utf-8').read()
 
     @app.route('/projects/<query>/comments/get/', methods=['GET'])
+    @crossdomain(origin='*')
     def commentsget(query):
           try:
             comments = Scratch.getProjComments(query, 3)
@@ -154,6 +202,7 @@ def run():
           return text
 
     @app.route('/projects/<query>/embed/')
+    @crossdomain(origin='*')
     def embed(query):
             try:
                   search = json.loads(str(Scratch.getInfo(query)).replace("'", '"'))
@@ -172,6 +221,7 @@ def run():
             return text
 
     @app.route('/projects/<query>/embed-light/')
+    @crossdomain(origin='*')
     def embed_light(query):
             try:
                   search = json.loads(str(Scratch.getInfo(query)).replace("'", '"'))
@@ -197,8 +247,9 @@ def run():
     def page_not_found(e):
       return open('./html/404.html', encoding='utf-8').read()
     
-    @app.route('/archive/')
-    def archive():
+    @app.route('/api/archive/')
+    @crossdomain(origin='*')
+    def archiveapi():
       arc = { 'dirs': [] }
       for dir in os.listdir('./projcache'):
             arc['dirs'].append(dir)
@@ -208,7 +259,8 @@ def run():
     def archive_home():
           return open('./html/archivehelp.html', encoding='utf-8').read()
 
-    @app.route('/archive/<query>/')
+    @app.route('/api/archive/<query>/')
+    @crossdomain(origin='*')
     def archive_dir(query):
       if os.path.isdir('./projcache/{}'.format(query)) == True:
             arc = { 'projects': [] }
@@ -220,7 +272,8 @@ def run():
       else:
             abort(404)
 
-    @app.route('/archive/<query>/<file>')
+    @app.route('/api/archive/<query>/<file>')
+    @crossdomain(origin='*')
     def archive_file(query, file):
       if os.path.isdir('./projcache/{}'.format(query)) == True and pathlib.Path('./projcache/{}/{}'.format(query, file)).exists() == True:
             return send_file('./projcache/{}/{}'.format(query, file))
@@ -228,6 +281,29 @@ def run():
             return redirect('https://youtu.be/xvFZjo5PgG0', 307)
       else:
             abort(404)
+
+    @app.route('/archive')
+    def archive():
+          return open('./html/archive.html').read()
+
+    @app.route('/archive/search/')
+    def archive_search():
+          args = request.args
+          if args.get('q') == '':
+                abort(404)
+          elif re.search('[@_!#$%^&*()<>?/\|}{~:]', args.get('q')):
+            return redirect('https://youtu.be/xvFZjo5PgG0', 307)
+          elif re.search('[a-zA-Z]', args.get('q')):
+                abort(404)
+          else:
+                if os.path.isdir('./projcache/{}'.format(args.get('q'))) == True:
+                      res = os.listdir('./projcache/{}'.format(args.get('q')))
+                      html = open('./html/getarchproject.html', encoding='utf-8').read()
+                      for file in res:
+                        html = html+'<a href="/api/archive/{}/{}">{}</a> <br>'.format(args.get('q'), file, file)
+                      return html.replace('//id', args.get('q'))+'</p> </body> </html>'
+                else:
+                      abort(404)
 
     server = WSGIServer((host, port), app)
     server.serve_forever()
